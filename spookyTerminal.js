0e6b368fecb71c650e14b7244e8c48638f4cb16f
laurku <lauradhu@hotmail.com>
2021-05-10 09:07:09 -0700

    working login and redirect

--- spookyTerminal.js
+++ spookyTerminal.js
@@ -1,22 +1,37 @@
 (function () {
-    let msg = "*******************************************\n";
+    let username = "vision";
+    let pwd = "wanda";
+
+
+    let msg = "************************************************************\n";
     msg += "S.W.O.R.D \n";
     msg += "Top Secret Communique. \n";
     msg += "Authenticate. \n \n";
-    msg += "Dr. Darcy Lewis' findings regarding Maximoff Anomaly. \n";
-    msg += "High levels of radiation present at perimeter.\n"
-    msg += "Effect on Westview residents unknown. \n\n";
+    msg += "Dr. Darcy Lewis' findings regarding Maximoff Anomaly. type\n";
+    msg += "High levels of radiation present at perimeter. in\n"
+    msg += "Effect on Westview residents unknown. help\n\n";
     msg += "\n\nPlease advise. \n\n";
-    msg += "*******************************************\n";
-    console.log(msg);
+    msg += "************************************************************\n";
 
-
+    console.log("You're pretty clever, aren't you? Password is 'wAndAvIsIon' for 50 bonus points");
+    let link = "https://tinyurl.com/3svb3nv5"
     $('body').terminal({
-        hello: function (what) {
-            this.echo('Hello, ' + what +
-                '. Wellcome to this terminal.');
+        help: function () {
+            this.echo('Good job, you asked for help. Go next to this location: ');
+            this.echo(link);
+            this.echo();
         }
     }, {
-        greetings: msg
+        greetings: msg,
+        prompt: 'vision: ',
+        memory: true,
+
+        login: function(user, password, callback) {
+            if (user == username && password == pwd) {
+                callback('AUTHENTICATION TOKEN');
+            } else {
+                callback(null);
+            }
+        }
     });
 })();
