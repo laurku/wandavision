e230f41a4f492148de9328ff26ee22bcd7341ce1
laurku <lauradhu@hotmail.com>
2021-05-10 08:42:33 -0700

    wandavision message to terminal

--- spookyTerminal.js
+++ spookyTerminal.js
@@ -1,10 +1,22 @@
 (function () {
+    let msg = "*******************************************\n";
+    msg += "S.W.O.R.D \n";
+    msg += "Top Secret Communique. \n";
+    msg += "Authenticate. \n \n";
+    msg += "Dr. Darcy Lewis' findings regarding Maximoff Anomaly. \n";
+    msg += "High levels of radiation present at perimeter.\n"
+    msg += "Effect on Westview residents unknown. \n\n";
+    msg += "\n\nPlease advise. \n\n";
+    msg += "*******************************************\n";
+    console.log(msg);
+
+
     $('body').terminal({
         hello: function (what) {
             this.echo('Hello, ' + what +
                 '. Wellcome to this terminal.');
         }
     }, {
-        greetings: 'Retro Vision Web Terminal'
+        greetings: msg
     });
 })();
